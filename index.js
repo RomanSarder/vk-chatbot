b9c494729985aa94213b6283b2ce75e8e4b8930a
Роман <roman.sarder@yandex.ru>
2017-04-23 14:20:59 +0300

    Launch notification when callback api procs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const VK = new VKAPI({
     token: "04916a8c74238b2065c4320c9d95de8806413bce7b28232d1cdfcd6f84fa2c7381ff6ebb3e01f325aea1e"
 });
 const PORT = process.env.PORT || 3000;
+const me = 425331872;
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -14,9 +15,9 @@ const PORT = process.env.PORT || 3000;
 // });
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-
+let mes;
 app.get('/', (req, res) => {
-    let mes;
+
     VK.call('messages.get', {
         out: 0,
     }).then((messages) => {
@@ -25,12 +26,11 @@ app.get('/', (req, res) => {
         console.log(mes.items[mes.items.length - 1].user_id);
         // res.send(JSON.stringify(messages));
         return VK.call('messages.send', {
-            // user_id: mes.items[mes.items.length - 1].user_id,
-            user_id: 153503782,
-            message: 'О, мне прислали сообщение. Спасибо'
+            user_id: mes.items[mes.items.length - 1].user_id,
+            message: 'Привет, я даже могу тебе вот так ответить!'
         });
     }).then(() => {
-        res.send('Message sent');
+        res.send(JSON.stringify(mes));
     }).catch(err => {
         console.log(err);
     })
@@ -41,7 +41,22 @@ app.post('/', (req, res) => {
     if (json.type === "confirmation" && (json.group_id === 145656698 || "145656698")) {
         res.send('b78abcff');
     }
-    res.send(JSON.stringify(req.body));
+    VK.call('message.send', {
+        user_id: me,
+        message: 'Я получил оповещение о сообщении'
+    }).then(() => {
+        return VK.call('message.send', {
+            user_id: me,
+            message: JSON.stringify(mes)
+        })
+    }).then(() => {
+        return VK.call('message.send', {
+            user_id: me,
+            message: JSON.stringify(json)
+        })
+    }).then(() => {
+        res.status(200).send('ok');
+    })
 });
 
 app.listen(PORT, () => {
